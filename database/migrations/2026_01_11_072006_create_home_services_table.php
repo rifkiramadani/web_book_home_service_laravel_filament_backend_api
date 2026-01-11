f53c0d89f3e4d3970057617847d09cde1eef9ef8
Muhammad Rifky Ramadani <rifkiramadani724@gmail.com>
2026-01-11 14:40:04 +0700

    feat: make model and migration

---
+++ database/migrations/2026_01_11_072006_create_home_services_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('home_services', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->string('thumbnail');
+            $table->text('about');
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->boolean('is_popular');
+            $table->unsignedBigInteger('price');
+            $table->unsignedBigInteger('duration');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('home_services');
+    }
+};
